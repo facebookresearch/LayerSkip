347bcc29e48ba1a2c17f96e1a0a3b46a0d32ea78
Mostafa Elhoushi <melhoushi@meta.com>
2024-05-07 19:12:07 +0000

    print and save sweep results

--- sweep.py
+++ sweep.py
@@ -1,4 +1,8 @@
 from typing import Dict, List
+import pandas as pd
+from datetime import datetime
+import os
+import tabulate
 
 from arguments import BenchmarkArguments, process_cli_arguments
 from benchmark import benchmark, load_model_and_tokenizer, setup
@@ -7,22 +11,30 @@ from self_speculation.generator_base import (
 )
 
 
-def sweep(benchmark_arguments: BenchmarkArguments, generation_config: GenerationConfig):
+def sweep(benchmark_arguments: BenchmarkArguments, generation_config: GenerationConfig, output_fname: str):
     results: List[Dict] = []
     setup(benchmark_arguments)
     model, tokenizer = load_model_and_tokenizer(benchmark_arguments)
-    for exit_layer in range(4, 32, 2):
-        for num_speculations in range(4, 14, 2):
+    for exit_layer in range(4, 20, 10):
+        for num_speculations in range(4, 12, 8):
             generation_config.exit_layer = exit_layer
             generation_config.num_speculations = num_speculations
 
             metric_result = benchmark(model, tokenizer, benchmark_arguments, generation_config)
 
             results.append({"exit_layer": exit_layer, "num_speculations": num_speculations, "time_per_token": metric_result['time_per_token']['mean']})
+            df = pd.DataFrame(results) 
+            # Update table every iteration
+            df.to_csv(output_fname, index=False)
             print(f"exit_layer: {exit_layer}, num_speculations: {num_speculations}, time_per_token: {metric_result['time_per_token']['mean']}")
 
-    print(results)
+    # Print summary table
+    print("\n")
+    header = results[0].keys()
+    rows =  [x.values() for x in results]
+    print(tabulate.tabulate(rows, header))
 
 if __name__ == "__main__":
     benchmark_arguments, generation_config = process_cli_arguments()
-    sweep(benchmark_arguments, generation_config)
+    os.makedirs("./logs/", exist_ok=True)
+    sweep(benchmark_arguments, generation_config, f"./logs/sweep_{datetime.now().strftime('%Y%m%d_%H%M%S')}.csv")
